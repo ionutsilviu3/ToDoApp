ae4253a97e63ff8991872189e8adfadf81bc03f2
ionutsilviu3 <ionut.boanc@student.unitbv.ro>
2022-11-17 08:30:51 +0200

    Created project

---
+++ src/test/java/com/boancionut/ToDoApp/ToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boancionut.ToDoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
